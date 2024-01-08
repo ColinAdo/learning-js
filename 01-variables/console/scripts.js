0189390351b1c39ce0c516edd4ab12cd3fb7b12a
ColinAdo <colinadodev30@gmail.com>
2024-01-08 06:15:26 +0300

    Initial commit

---
+++ 01-variables/console/scripts.js
@@ -0,0 +1,22 @@
+console.log(100);
+
+console.log("Hello  world!");
+
+console.log(10, "Name", false);
+
+console.table({ name: 'Colin', age: 30 })
+
+console.error("Error");
+
+console.warn("Warnings");
+
+console.group('Greetings');
+console.log("Hello  world!");
+console.log("Hello  world!");
+console.log("Hello  world!");
+console.log("Hello  world!");
+console.groupEnd();
+
+const styles = 'padding: 10px; color: brown; font-size: 30px; background-color: white; font-weight: bold'
+
+console.log('%cThis is Js', styles);
