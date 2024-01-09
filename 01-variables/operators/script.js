43ac8a0824dcc52d463b7ff8120465e578a01bbb
ColinAdo <colinadodev30@gmail.com>
2024-01-09 18:24:22 +0300

    Working with operators

---
+++ 01-variables/operators/script.js
@@ -0,0 +1,16 @@
+// Comparision operators
+
+let x;
+x = 2 == 2; // The == compares the value only
+console.log(x);
+x = 2 == '2'
+console.log(x);
+
+let y;
+y = 3 === 3; // The == compares the value and the type of the value
+console.log(y);
+y = 3 === '3';
+console.log(y);
+y = 3 === parseInt('3');
+console.log(y);
+
