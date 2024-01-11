ef856fb0cad37cbc2f105431e5462e96ed136f1f
ColinAdo <colinadodev30@gmail.com>
2024-01-11 18:12:03 +0300

    Working with Math challenge

---
+++ 01-variables/math-challenge/script.js
@@ -0,0 +1,26 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+console.log(x, y);
+
+// Sum
+const sum = x + y;
+const sunResult = `Sum ${x} + ${y} = ${sum}`
+console.log(sunResult);
+
+// Subtructon
+const difference = x - y;
+const differenceResult = `Subtruction ${x} - ${y} = ${difference}`
+console.log(differenceResult);
+
+// Division
+const quotient = x / y;
+const quotientResult = `Division ${x} / ${y} = ${quotient}`
+console.log(quotientResult);
+
+// Reminder
+const remainder = x % y;
+const remainderResult = `Reminder ${x} / ${y} = ${remainder}`
+console.log(remainderResult);
+
+
