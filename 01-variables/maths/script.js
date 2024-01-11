636d148fc38bfc2ef0cc7a895844b4116fca3947
ColinAdo <colinadodev30@gmail.com>
2024-01-11 15:12:59 +0300

    Working with Math object

---
+++ 01-variables/maths/script.js
@@ -0,0 +1,35 @@
+let x;
+
+x = Math;
+
+console.log(x);
+
+x = Math.min(1, 4, 6);
+console.log(x);
+
+x = Math.max(2, 5, 0);
+console.log(x);
+
+x = Math.pow(2, 2);
+console.log(x);
+
+x = Math.sqrt(4);
+console.log(x);
+
+x = Math.ceil(5.2); // Always rounds up
+console.log(x);
+
+x = Math.round(5.2); // Rounds up based on number after the decimal place.
+console.log(x);
+
+x = Math.floor(5.9); // Rounds down 
+console.log(x);
+
+x = Math.random();
+console.log(x);
+
+x = Math.random() * 10 + 1;  // Produces number btn 0 to 10 in decimal places
+console.log(x);
+
+x = Math.floor(Math.random() * 10 + 1);  // Produces number btn 0 to 10 in decimal places
+console.log(x);
