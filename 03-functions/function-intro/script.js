997b4fc0eabba49c13264e8b4e5ec24e7f8dad20
ColinAdo <colinadodev30@gmail.com>
2024-01-13 11:42:35 +0300

    Working with functions

---
+++ 03-functions/function-intro/script.js
@@ -0,0 +1,17 @@
+// Defining a function
+function greetings() {
+    console.log("Hello, world!");
+}
+
+greetings(); // Calling a function
+
+function sum(a, b) { // a, b are called parameters
+    console.log(a + b);
+}
+sum(2, 3); // 2, 3 are called arguments
+
+function sub(x, y) {
+    return x - y;
+}
+const ans = sub(14, 1);
+console.log(ans);
