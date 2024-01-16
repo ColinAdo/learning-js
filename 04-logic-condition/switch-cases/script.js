b7320ace1f57ae3ea447d602e23d69004381e00e
ColinAdo <colinadodev30@gmail.com>
2024-01-16 21:24:45 +0300

    Working with switch case

---
+++ 04-logic-condition/switch-cases/script.js
@@ -0,0 +1,31 @@
+// Switch cases
+const d = new Date();
+const day = d.getDay() + 5; 
+
+
+switch (day) {
+    case 1:
+        console.log('Today is Sunday');
+        break;
+    case 2: 
+        console.log('Today is Monday');
+        break;
+    case 3:
+        console.log('Today is Tuesday');
+        break;
+    case 4: 
+        console.log('Today is Wednesday');
+        break;
+    case 5: 
+        console.log('Today is Thursday');
+        break;
+    case 6: 
+        console.log('Today is Friday');
+        break;
+    case 7: 
+        console.log('Today is Saturday');
+        break;
+    
+    default:
+        break;
+}
