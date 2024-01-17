24e87b4b4861a292f0fb69f9f3d39c6206bc9c88
ColinAdo <colinadodev30@gmail.com>
2024-01-17 23:12:30 +0300

    Working with Do-While and While loops

---
+++ 04-logic-condition/do-while-while-loop/script.js
@@ -0,0 +1,25 @@
+// While Loop 
+
+let i = 1;
+
+// while (i <= 10) { 
+//     console.log(i);
+//     i++;
+// }
+
+// Nested while loop
+// while (i <= 10) { 
+//     let j = 1;
+//     while (j<=10) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//         j++;
+//     }
+//     i++;
+// }
+
+// Do-While loop
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
