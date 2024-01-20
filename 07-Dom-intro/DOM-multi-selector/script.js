50dfbb64b023523544db428fa66400b9ccb6298e
ColinAdo <colinadodev30@gmail.com>
2024-01-20 08:30:51 +0300

    Working with DOM multiple selectors functions

--- 07-Dom-intro/DOM-multi-selector/script.js
+++ 07-Dom-intro/DOM-multi-selector/script.js
@@ -1,24 +1,42 @@
-// 1. getElementById()
-// Getting / changing attributes
-console.log(document.getElementById('title'));
-console.log(document.getElementById('title').innerText);
-console.log(document.getElementById('title').getAttribute('id'));
+// There are three ways to get mutiple items:
+// 1. querySectorAll() - Returns a NodeList that can be iterated over
 
-// Changing styles
-const item = document.getElementById('title');
-item.style.backgroundColor = 'black';
-item.style.color = 'white';
-item.style.padding = '10px';
-item.style.borderRadius = '10px';
+const items = document.querySelectorAll('.item');
+console.log(items);
 
-// QuerySelector()
-const query = document.querySelector('#shopping-list');
-console.log(query);
+items.forEach((item, index) => {
+    item.style.color = 'red'
 
-console.log(document.querySelector('input[type="text"]'));
+    // if (index === 0) { 
+    //     item.remove();
+    // }
+    if (index === 2) {
+        item.innerHTML = `Honey <a href="">x</a>`;
+    }
 
-console.log(document.querySelector('.container'));
+});
 
-const firstItem = document.querySelector('li:nth-child(1)');
-firstItem.style.color = 'red';
-firstItem.style.fontWeight = 'bold';
+/* 2. getElementByClassName() -Returns HTMLCollection
+ which can be converted toarray first before iterating through
+ */
+
+const elements = document.getElementsByClassName('item');
+console.log(elements.length);
+console.log(elements[0]);
+
+const itmes2 = Array.from(elements)
+itmes2.forEach((item,) => {
+    item.style.color = 'green';
+});
+
+/* 2. getElementByTagName() -Returns HTMLCollection
+ which can be converted to array first before iterating through
+ */
+
+const elements2 = document.getElementsByTagName('li');
+console.log(elements[2]);
+
+const items3 = Array.from(elements)
+items3.forEach((element) => { 
+    element.style.color = 'blue';
+})
